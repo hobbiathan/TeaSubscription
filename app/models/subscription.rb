c58f7c5a844389b327e51b1c168a391a82202e67
Ruben Pasillas <hobbiathan@gmail.com>
2022-06-01 15:54:58 -0600

    update models

---
+++ app/models/subscription.rb
@@ -0,0 +1,9 @@
+class Subscription < ApplicationRecord
+  belongs_to :customer
+
+  validates_presence_of :title,
+                       :price,
+                       :status,
+                       :frequency
+
+end
