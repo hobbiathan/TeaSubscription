a72ebc3e14c31d4ecaba4ffcb70656d897c6acef
Ruben Pasillas <hobbiathan@gmail.com>
2022-06-06 12:24:26 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tea.create(title: "Matcha", description: "Good 'ol Matcha Tea", temperature: 90, brew_time: "60")
+Tea.create(title: "Yerba Mate", description: "Good 'ol Yerba Mate", temperature: 100, brew_time: "120")
+Tea.create(title: "Green Tea", description: "Good 'ol Green Tea", temperature: 85, brew_time: "70")
